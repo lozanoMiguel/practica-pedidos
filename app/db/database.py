cb0705262eb79fb0b1bd4233b99974694b7eb510
miguel <lozanomiguel92@gmail.com>
2026-07-21 16:17:01 +0200

    modelos y bdd finalizadas

---
+++ app/db/database.py
@@ -0,0 +1,8 @@
+from typing import List
+from app.models.pedido import Pedido
+from app.models.producto import Producto
+
+pedidos_db: List[Pedido]
+producto_db: List[Producto]
+contador_pedido = 1
+contador_producto = 1
--- app/models/pedido.py
+++ app/models/pedido.py
@@ -1,6 +1,6 @@
 from pydantic import BaseModel, Field, EmailStr
 from typing import Optional, Literal, List
-from app.models.item_pedido import Item_pedido
+from app.models.item_pedido import ItemPedido
 
 class PedidoCreate(BaseModel):
     cliente_nombre: str = Field(..., min_length=3, max_length=100)
@@ -8,10 +8,12 @@ class PedidoCreate(BaseModel):
     estado: Optional[Literal["pendiente", "confirmado", "enviado", "entregado", "cancelado"]] = Field(default="pendiente")
     
 class PedidoUpdate(BaseModel):
-    pass
+    cliente_nombre: Optional[str] = Field(None, min_length=3, max_length=100)
+    cliente_email: Optional[EmailStr]
+    estado: Optional[Literal["pendiente", "confirmado", "enviado", "entregado", "cancelado"]] = Field(default=None)
 
 class Pedido(PedidoCreate):
     id: int
     fecha_creacion: str
     total: float
-    productos: Optional[List[Item_pedido]] = []
+    productos: Optional[List[ItemPedido]] = []
