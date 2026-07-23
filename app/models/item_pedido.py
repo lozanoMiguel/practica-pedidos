92c51061a80739d6d1348920260c543454b748fd
miguel <lozanomiguel92@gmail.com>
2026-07-23 11:04:31 +0200

    endpoints de productos terminados

--- app/models/item_pedido.py
+++ app/models/item_pedido.py
@@ -4,10 +4,7 @@ from typing import Optional
 class ItemPedidoCreate(BaseModel):
     producto_id: int
     cantidad: int = Field(..., gt=0)
-    precio_unitario: float 
-    subtotal: float
     
-
 class ItemPedido(ItemPedidoCreate):
     producto_id: int
     cantidad: int 
--- app/routes/productos.py
+++ app/routes/productos.py
@@ -1,6 +1,6 @@
 from fastapi import HTTPException, APIRouter
 from typing import Optional, List
-from app.models.producto import Producto, ProductoCreate
+from app.models.producto import Producto, ProductoCreate, ProductoUpdate
 from app.db.database import producto_db, contador_producto
 router = APIRouter()
 
@@ -27,17 +27,42 @@ def obtener_todos_los_productos(categoria: Optional[str]=None, stock_minimo: Opt
 
 @router.post("/", response_model=Producto, status_code=201)
 def agregar_producto(producto_nuevo: ProductoCreate):
-    global producto_db
     global contador_producto
     nuevo = Producto(**producto_nuevo.model_dump(),id=contador_producto)
     contador_producto += 1
     producto_db.append(nuevo)
     return nuevo
 
-@router.get("/{id}", response_model=Producto, status_code=201)
+@router.get("/{id}", response_model=Producto, status_code=200)
 def obtener_producto(id: int):
-    global producto_db
     resultado = next((p for p in producto_db if p.id == id), None)
     if resultado is None:
         raise HTTPException(404, "No existe un producto con ese ID")
     return resultado
+
+@router.put("/{id}", response_model=Producto, status_code=200)
+def actualizar_producto(id: int, producto_acualizado: ProductoUpdate):
+    for i,p in enumerate(producto_db):
+        if p.id == id:
+            cambios_dic = producto_acualizado.model_dump(exclude_unset=True)
+            producto = p.model_copy(update=cambios_dic)
+            producto_db[i] = producto
+            return producto
+    raise HTTPException(404, "El producto asociado a ese ID no existe")
+    
+@router.delete("/{id}", status_code=204)
+def eliminar_producto(id: int):
+    producto_a_eliminar = next((p for p in producto_db if p.id == id), None)
+    if producto_a_eliminar is None:
+        raise HTTPException(404, f"El producto asociado al id: {id} no existe")
+    producto_db.remove(producto_a_eliminar)
+    return 
+
+@router.patch("/{id}/stock", response_model=Producto, status_code=200)
+def actualizar_stock(id: int, nuevo_stock:int):
+    for i,p in enumerate(producto_db):
+        if p.id == id:
+            producto_db[i].stock = nuevo_stock
+            return p
+            
+    raise HTTPException(404, "El producto asociado a ese ID no existe")
