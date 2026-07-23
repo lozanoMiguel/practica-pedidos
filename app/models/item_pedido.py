f7f8ce232e8521b0cf8f2f09d38e15e86e3eb11b
miguel <lozanomiguel92@gmail.com>
2026-07-23 09:03:20 +0200

    modificando clase itempedido

--- app/models/item_pedido.py
+++ app/models/item_pedido.py
@@ -2,14 +2,14 @@ from pydantic import BaseModel, Field
 from typing import Optional
 
 class ItemPedidoCreate(BaseModel):
+    producto_id: int
     cantidad: int = Field(..., gt=0)
     precio_unitario: float 
     subtotal: float
     
-class ItempedidoUpdate(BaseModel):
-    cantidad: Optional[int] = Field(None, gt=0)
-    precio_unitario: Optional[float]
-    subtotal: Optional[float]
-    
+
 class ItemPedido(ItemPedidoCreate):
     producto_id: int
+    cantidad: int 
+    precio_unitario: float 
+    subtotal: float
